2d677506334853ebce07b0bc52142b03eb7bb011
Natalie Blackburn & Corey Norris <student@epicodus.com>
2014-04-18 11:54:24 -0700

    create factory for user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,18 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+    sequence :name do |n|
+    Faker::Internet.user_name
+  end
+
+    sequence :email do |n|
+    Faker::Internet.safe_email
+  end
+
   factory :user do
+    name
+    email
+    password 'foobarbaz'
+    password_confirmation 'foobarbaz'
   end
 end
