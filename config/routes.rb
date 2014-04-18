51fe37c9f7e27d9ae7fc9e2881374425627ce01d
Natalie Blackburn & Corey Norris <student@epicodus.com>
2014-04-18 12:25:30 -0700

    add devise functionality and alter devise form styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 BloggerClone::Application.routes.draw do
-  devise_for :users
+
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   resources :posts
   root to: "posts#index"
 end
