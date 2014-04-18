2373116e2e4bf6ed96e33a379b4b5b6f5b99559e
Natalie Blackburn & Corey Norris <student@epicodus.com>
2014-04-18 09:49:16 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+=Blogsite Clone
+
+* As a user I want to be able to create an account and sign in.
+
+* As a user I want to be able to comment on questions, and edit and delete my own comments.
+
+* As an author I want to be able to write, edit, delete my blog posts.
+
+* As an author I want to be able to delete others comments on my posts.
+
+* As a administrator I want to be able to edit and delete posts and comments.
+
+* As an administrator I want to be able to send a welcome email to new users.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BloggerClone::Application.config.secret_key_base = '2ab26272b1012fdbaa56fbc85f54f641d4499c944d9d0a717dcc1275bc1dde66ecb913111ed73f341e991e69e6c11a671cb9bb3042221953699e3e32decfccae'
